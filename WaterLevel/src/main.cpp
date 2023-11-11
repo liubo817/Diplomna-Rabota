acededef42ceb34cc5054e60ed7af4f35d75cc70
liubo817 <lyubomir.y.stanev.2019@elsys-bg.org>
2023-11-11 14:01:29 +0200

    Added percantages to the water level sensor as well as working with a LED

--- WaterLevel/src/main.cpp
+++ WaterLevel/src/main.cpp
@@ -2,13 +2,14 @@
 
 #define sensorPower 21
 #define sensorPin 2
-
-// Value for storing water level
-int val = 0;
+#define led 25
+int water_level;
+int water_level_percent;
 
 void setup() {
 	// Set D7 as an OUTPUT
 	pinMode(sensorPower, OUTPUT);
+  pinMode(led, OUTPUT);
 	
 	// Set to LOW so no power flows through the sensor
 	digitalWrite(sensorPower, LOW);
@@ -16,7 +17,7 @@ void setup() {
 	Serial.begin(9600);
 }
 
-int readSensor() {
+int readSensor(int val) {
 	digitalWrite(sensorPower, HIGH);	// Turn the sensor ON
 	delay(10);							// wait 10 milliseconds
 	val = analogRead(sensorPin);		// Read the analog value form sensor
@@ -24,12 +25,24 @@ int readSensor() {
 	return val;							// send current reading
 }
 
+int sensor_to_percent(int level)
+{
+  int sensor_data = map(level, 0, 1400, 0, 100);
+  return sensor_data;
+}
+
 void loop() {
 	//get the reading from the function below and print it
-	int level = readSensor();
-	
-	Serial.print("Water level: ");
-	Serial.println(level);
+  water_level_percent = sensor_to_percent(readSensor(water_level));
+  Serial.print("Water level percentage: ");
+  Serial.println(water_level_percent);
+	if (water_level_percent <= 85)
+  {
+    digitalWrite(led, HIGH);
+  }else{
+    digitalWrite(led, LOW);
+  }
+
 	
 	delay(1000);
 }
