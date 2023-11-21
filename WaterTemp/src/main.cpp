74c104befe8882b8124c997601c26fd00e810fbf
liubo817 <lyubomir.y.stanev.2019@elsys-bg.org>
2023-11-21 13:02:10 +0200

    Tested temperature sensor ds18b20 in water - working properly

---
+++ WaterTemp/src/main.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into digital pin 2 on the Arduino
+#define ONE_WIRE_BUS 4
+
+// Setup a oneWire instance to communicate with any OneWire device
+OneWire oneWire(ONE_WIRE_BUS);	
+
+// Pass oneWire reference to DallasTemperature library
+DallasTemperature sensors(&oneWire);
+
+void setup(void)
+{
+  sensors.begin();	// Start up the library
+  Serial.begin(9600);
+}
+
+void loop(void)
+{ 
+  // Send the command to get temperatures
+  sensors.requestTemperatures(); 
+
+  //print the temperature in Celsius
+  Serial.print("Temperature: ");
+  Serial.print(sensors.getTempCByIndex(0));
+  Serial.print("C");
+  Serial.println();
+  
+  delay(500);
+}
